74bf198ef0ad11326670ff18e54bbd080c25eeb4
Elaine Smith <oneile1219@gmail.com>
2022-05-12 13:54:40 -0700

    two sum func and tests

--- functions/two_sum.py
+++ functions/two_sum.py
@@ -1,2 +1,6 @@
 def twoSum(n, t):
-    pass
+    '''Finds which two numbers in a list of numbers add up to the given target.'''
+    for i in range(len(n)):
+        for j in range(i + 1, len(n)):
+            if n[j] == t - n[i]:
+                return [i, j]
